1a66d7ac2aa22adeb5ca14e38cafa947818f6568
Yagnasri21 <yagnashri06@gmail.com>
2025-09-25 12:41:13 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -8,66 +8,19 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.blue[300],
         appBar: AppBar(
-            backgroundColor: Colors.red, title: const Text("Welcome Screen")),
-        body: Container(
-          child: const Center(
-            child: Text("Hello world!"),
-          ),
+          backgroundColor: Colors.pinkAccent,
+          title: const Text("Welcome to the UI-FLUTTER LAB USING DART"),
         ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        body: const Center(
+          child: Text("Namaskaram🙏 LOKANIKI SWAGATHAM YELLUNDI KOSAM "),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
